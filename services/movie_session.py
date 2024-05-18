6b5a16f7a2461e5715c96e14ded6871e1d64d200
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-05-18 15:30:48 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -19,7 +19,7 @@ def create_movie_session(
 
 def get_movies_sessions(session_date: str = None) -> QuerySet:
     query = MovieSession.objects.all()
-    if query:
+    if session_date:
         query = query.filter(show_time__date=session_date)
     return query
 
@@ -34,7 +34,7 @@ def update_movie_session(
         movie_id: int = None,
         cinema_hall_id: int = None
 ) -> MovieSession:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
@@ -46,4 +46,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=session_id).delete()
+    return get_movie_session_by_id(session_id).delete()
